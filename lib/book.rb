216fbc768728c99844666b18af89c6e14dfeffc8
Rocio Batres <jonasgirl21@gmail.com>
2020-09-18 20:28:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,4 +17,24 @@ def author
   @author 
 end 
 
+def page_count=(num)
+  @page_count = num 
+end 
+
+def page_count 
+  @page_count
+end  
+
+def genre=(genre)
+  @genre = genre 
+end 
+
+def genre
+  @genre 
+end   
+  
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end   
+
 end 
