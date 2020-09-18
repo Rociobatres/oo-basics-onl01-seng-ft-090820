5db7c449a0f68e81f220615a5397ceb474ee2ecf
Rocio Batres <jonasgirl21@gmail.com>
2020-09-18 20:39:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,14 @@
 class Shoe 
   
-  
+attr_accessor :color :size :material :condition 
+attr_reader :brand 
+
+def intialization(brand)
+@brand = brand
+end 
+
+def cobble(condition)
+  puts "shoe had been repaired"
+end   
 
 end   
